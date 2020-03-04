d0f1abf6860a19e862ca9a3f3e85d4fbcbbe57a4
Igor Zelenov <igorg.zelenov@gmail.com>
2020-03-04 18:02:35 -0500

    first commit

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,9 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+   title = {type: String, required: true, unique: true},
+   level = {type: String, enum: ['Easy Peasy']},
+   ingredients = {enum:[]}
+   cuisine = 
+   dishType = 
+   let 
+
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 module.exports = Recipe;
